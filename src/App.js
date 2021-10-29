11c6df183453cf13f446a4b9d30349f700d66181
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2021-10-29 14:05:43 -0300

    call to api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
+import { useState } from 'react'
 import { Formik, Form, Field } from 'formik'
 
 import './header.css'
 
 const App = () => {
+  const [photos, setPhotos] = useState([])
+
+  console.log(photos)
+
   return (
     <div>
       <header>
         <Formik
           initialValues={{ search: '' }}
           onSubmit={async (values) => {
-            // call unsplash api
-            console.log(values)
+            const response = await fetch(
+              `https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
+              {
+                headers: {
+                  Authorization: `Client-ID ${process.env.REACT_APP_API_KEY}`,
+                },
+              },
+            )
+            const data = await response.json()
+
+            setPhotos(data.results)
           }}
         >
           <Form>
